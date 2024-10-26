b90adac89884f2deb352470bcb5dbe4abb67e13a
jyothirmyivasa <151708381+jyothirmyivasa@users.noreply.github.com>
2024-10-26 12:37:59 +0530

    Create StringDemo.java

---
+++ StringDemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.String;
+public class StringDemo{
+	public static void main(String args[]){
+		String s1="welcome";
+		char ch[]={'J','Y','O','T','H','I'};
+		String s2=new String(ch);
+		String s3=new String("Example");
+		System.out.println("The string s1 is "+s1);
+		System.out.println("String s2 is "+s2);
+		System.out.println("String s3 is "+s3);
+	}
+}
